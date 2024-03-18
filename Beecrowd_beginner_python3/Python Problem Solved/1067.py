726202d7644b73912a616c723aa61a95e1f4306b
Anayet Ullah Efty <57069481+anayetullahefty@users.noreply.github.com>
2024-03-18 11:23:37 +0600

    1067.py

---
+++ Beecrowd_beginner_python3/Python Problem Solved/1067.py
@@ -0,0 +1,16 @@
+# 1067 | Odd Numbers
+
+for i in range (1, int(input())+1,2):
+    print(i)
+
+''' 
+for i in range(1, int(input())+1):
+    if i % 2 == 1:
+        print(i)
+'''
+
+''' 
+for i in range(1, int(input())+1):
+    if i % 2 != 0:
+        print(i)
+'''
