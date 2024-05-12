b64ccfdab39b794d6d947a8d88cf5cdd1ed4a46b
Anayet Ullah Efty <57069481+anayetullahefty@users.noreply.github.com>
2024-05-12 15:09:20 +0600

    1177.py

---
+++ Beecrowd_beginner_python3/Python Problem Solved/1177.py
@@ -0,0 +1,14 @@
+#1177 - Array Fill II
+T = int(input())
+j = 0
+
+for i in range(1000):
+
+    if j < T:
+        print(f"N[{i}] = {j}")
+        j += 1
+    
+    else:
+        j = 0
+        print(f"N[{i}] = {j}")
+        j += 1
